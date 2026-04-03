74ea52d9107038996bc0c1fbf18ba5e9a1ae5852
AlinaRubila <angryagnstfanfic412@mail.ru>
2026-04-03 16:31:32 +0400

    added door creek and fixed flashlight position

---
+++ Assets/Scripts/Door.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class Door : MonoBehaviour
+{
+    [SerializeField] AudioSource source;
+    [SerializeField] HingeJoint hinge;
+    float minVelocity = 0.5f;
+    float previousAngle = 0f;
+    float velocity;
+    private void Update()
+    {
+        velocity = Mathf.Abs(hinge.velocity);
+        if (velocity > minVelocity)
+        {
+            if (!source.isPlaying) source.Play();
+            source.volume = Mathf.Clamp01(velocity / 100f);
+            source.pitch = 0.8f + velocity / 200f;
+        }
+        else
+        {
+            if (source.isPlaying) source.Stop();
+        }
+    }
+
+}
